029f999ee2db69d9262f5ee79bed340f6becdb53
ShahzaibaslamIT <aslamshazaib@gmail.com>
2024-06-26 14:40:44 +0500

    This is a Word-Counter

---
+++ main.ts
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+import chalk from "chalk";
+
+console.log(chalk.bold.bgCyanBright("\n\t\t Welcome to my Word Counter!"));
+
+const answers:{
+    Sentence:string
+
+} = await inquirer.prompt([{
+
+    name:"Sentence",
+    type: "input",
+    message:"Enter your sentence to count the word"
+}])
+
+const words =answers.Sentence.trim().split(" ")
+
+console.log(words)
+console.log(`Your sentence word count is ${words.length}`);
